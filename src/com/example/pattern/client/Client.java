9b3f2405c671f83c1a87fc493c3db605f1154320
Phat Ngo <ntp18799@gmail.com>
2022-04-26 15:06:50 +0700

    first commit

---
+++ src/com/example/pattern/client/Client.java
@@ -0,0 +1,12 @@
+package com.example.pattern.client;
+
+import com.example.pattern.model.Computer;
+
+public class Client {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Computer computer1 = new Computer("Window 10", "Word 2013", "BKAV", "Chrome v69", "Skype");
+        Computer computer2 = (Computer) computer1.clone();
+        computer2.setOthers("Skype, Teamviewer, FileZilla Client");
+        System.out.println(computer2);
+    }
+}
